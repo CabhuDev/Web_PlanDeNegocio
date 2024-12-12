678d70be875f0cd30aff88b9388da3773a64f007
CabhuDev <pabloch_91@hotmail.com>
2024-12-12 19:22:28 +0100

    index updated

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Función para alternar la visibilidad del menú de navegación.
+ */
+function toggleMenu() {
+    // Selecciona el elemento con la clase 'navbar'
+    const navbar = document.querySelector('.navbar');
+    
+    // Alterna la clase 'show' en el elemento 'navbar'
+    navbar.classList.toggle('show');
+}
